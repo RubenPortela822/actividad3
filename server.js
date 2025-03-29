011bf32c744c67625a87714035567ed0888c3b29
Ruben Portela <portela652@gmail.com>
2025-03-29 14:18:46 -0500

    Se agregan comentarios para aclarar el funcionamiento

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
+// Estas son las importaciones necesarias para el proyecto
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 
+// se instancia y se crea la app y el servidor socket
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// declaro publica la carpeta public poder servir el html dentro de esta carpeta
 app.use(express.static('public')); 
 
+//creo el escuchador de eventos
 io.on('connection', (socket) => {
-    console.log('Nuevo usuario conectado');
-
+    
+    //listener que tienen que llamar los clientes para suscribirse  una sala de chat
     socket.on('joinRoom', (room) => {
-        socket.join(room); 
-        
+        socket.join(room);         
         socket.to(room).emit('message', `Un nuevo usuario se ha unido a la sala ${room}`);
     });
 
+    // listener que recepciona y emite los mensajes a la sala correspondiente
     socket.on('message', ({ room, message }) => {
         io.to(room).emit('message', message);        
     });
 
+    //listener que se llama para cancelar la suscripcion a la sala cuando se recarga la pagina o se cierra la pagina
     socket.on('disconnect', () => {
         console.log('Usuario desconectado');
     });
@@ -29,3 +34,4 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('Servidor corriendo en http://localhost:3000');
 });
+// 
